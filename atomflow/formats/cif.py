408fe940685994fd59004a1ee136a2ee523e8797
bjtha <142571425+bjtha@users.noreply.github.com>
2026-01-10 21:37:54 +0000

    Complete CIF file reading methods

--- atomflow/formats/cif.py
+++ atomflow/formats/cif.py
@@ -1,9 +1,8 @@
-import pathlib
 from collections import defaultdict
 import os
 from typing import Iterable
 
-from atomflow.aspects import *
+from atomflow.components import *
 from atomflow.atom import Atom
 from atomflow.formats import Format
 
@@ -24,15 +23,69 @@ class CIFFormat(Format):
 
     extensions = (".cif", ".mmcif")
 
+    field_cmp_mapping = {
+        "id": IndexComponent,
+        "label_alt_id": AltLocComponent,
+        "label_asym_id": ChainComponent,
+        "label_atom_id": NameComponent,
+        "label_seq_id": ResIndexComponent,
+        "type_symbol": ElementComponent,
+        "Cartn_x": CoordXComponent,
+        "Cartn_y": CoordYComponent,
+        "Cartn_z": CoordZComponent,
+        "occupancy": OccupancyComponent,
+        "pdbx_PDB_ins_code": InsertionComponent,
+        "B_iso_or_equiv": TemperatureFactorComponent,
+        "pdbx_formal_charge": FormalChargeComponent,
+    }
+
     @classmethod
     def read_file(cls, path: str | os.PathLike) -> list[Atom]:
 
-        tables = cls._extract_tables(path)
-        entities = tables["_entity"]
+        tables = cls._extract_tables(path, names=("_entity", "_atom_site"))
 
+        entity_table = tables["_entity"]
+        atom_table = tables["_atom_site"]
 
+        num_rows = len(atom_table["id"])
 
-        return []
+        atoms = []
+
+        for atom_i in range(num_rows):
+
+            cmps = []
+
+            values = [col[atom_i] for col in atom_table.values()]
+            for field_name, value in zip(atom_table, values):
+
+                # Skip unknown/placeholder values
+                if value in "?.":
+                    continue
+
+                # For cases with simple 1:1 mapping between data item (field) and component type
+                elif field_name in cls.field_cmp_mapping:
+                    cmp_type = cls.field_cmp_mapping[field_name]
+                    cmps.append(cmp_type(value))
+
+                elif field_name == "label_comp_id":
+                    if value in AA_RES_TO_SYM:
+                        cmps.append(AAResidueComponent(value))
+                    elif value in DNA_RES_TO_SYM:
+                        cmps.append(DNAResidueComponent(value))
+                    elif value in RNA_RES_CODES:
+                        cmps.append(RNAResidueComponent(value))
+                    else:
+                        cmps.append(ResidueComponent(value))
+
+                # Get entity name from table
+                elif field_name == "label_entity_id":
+                    entity_row = cls._get_row_by_id(entity_table, int(value))
+                    entity_name = entity_row["pdbx_description"]
+                    cmps.append(EntityComponent(entity_name))
+
+            atoms.append(Atom(*cmps))
+
+        return atoms
 
     @classmethod
     def to_file(cls, atoms: Iterable[Atom], path: str | os.PathLike) -> None:
@@ -72,9 +125,9 @@ class CIFFormat(Format):
         return parts
 
     @classmethod
-    def _extract_tables(cls, path: str | os.PathLike) -> dict:
+    def _extract_tables(cls, path: str | os.PathLike, names: None | Iterable[str] = None) -> dict:
 
-        """Reads only the table information from a cif file."""
+        """Reads the table information from a cif file. Optionally only extract tables with given names."""
 
         with open(path, "r") as file:
             lines = (ln.strip() for ln in file.readlines())
@@ -96,6 +149,9 @@ class CIFFormat(Format):
             elif in_table:
                 if line.startswith("_"):
                     cat, field = line.split(".")
+                    if names and cat not in names:
+                        in_table = False
+                        continue
                     tables[cat][field] = []
                     num_cols = len(tables[cat])
                 else:
@@ -130,4 +186,5 @@ class CIFFormat(Format):
 
 
 if __name__ == '__main__':
-    CIFFormat.read_file("../../tests/data/cif/1A52.cif")
+    atoms = CIFFormat.read_file("../../tests/data/cif/1A52.cif")
+    print(f"{len(atoms)} read")
